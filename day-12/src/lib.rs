0cb3dced2036153b8de88f0bfa5fbb6cbbcb7866
Bram Buijs <bdbuijs@gmail.com>
2024-04-12 15:11:29 +0200

    Finished day-12

--- day-12/src/lib.rs
+++ day-12/src/lib.rs
@@ -1,3 +1,5 @@
+use std::collections::HashMap;
+
 use nom::{
     character::complete::{char as nomchar, newline, one_of, u8 as nom_u8},
     multi::{many1, separated_list1},
@@ -8,12 +10,9 @@ use nom::{
 pub fn process_part1(input: &str) -> String {
     let (input, rows) = parse_input(input).unwrap();
     assert!(input.is_empty());
+    let mut cache = HashMap::new();
     rows.into_iter()
-        .map(|(mut springs, groups)| {
-            let placeable = groups.iter().sum::<u8>()
-                - springs.iter().filter(|s| s == &&Spring::Damaged).count() as u8;
-            solve(&mut springs, placeable, 0, &groups)
-        })
+        .map(|record| count_solutions(&mut cache, &record))
         .sum::<usize>()
         .to_string()
 }
@@ -21,96 +20,138 @@ pub fn process_part1(input: &str) -> String {
 pub fn process_part2(input: &str) -> String {
     let (input, rows) = parse_input(input).unwrap();
     assert!(input.is_empty());
-    "".to_string()
+    let mut cache = HashMap::new();
+    rows.into_iter()
+        .map(|record| count_solutions(&mut cache, &record.unfold()))
+        .sum::<usize>()
+        .to_string()
 }
 
-#[derive(Debug, PartialEq, Eq, Clone)]
+#[derive(Debug, PartialEq, Eq, Clone, Hash)]
 enum Spring {
     Operational, // '.'
     Damaged,     // '#'
     Unknown,     // '?'
 }
 
-fn solve(springs: &mut Vec<Spring>, placeable: u8, solutions: usize, groups: &[u8]) -> usize {
-    if placeable == 0 {
-        let solved = is_solved(springs, groups);
-        if solved {
-            return solutions + 1;
+impl From<char> for Spring {
+    fn from(value: char) -> Self {
+        match value {
+            '.' => Self::Operational,
+            '#' => Self::Damaged,
+            '?' => Self::Unknown,
+            c => panic!("Unexpected character: {c}"),
+        }
+    }
+}
+
+#[derive(Debug, PartialEq, Eq, Clone, Hash)]
+struct Record {
+    springs: Vec<Spring>,
+    groups: Vec<u8>,
+}
+
+impl Record {
+    fn new(springs: Vec<Spring>, groups: Vec<u8>) -> Self {
+        Self { springs, groups }
+    }
+
+    fn next_spring(&self) -> Self {
+        Self {
+            springs: self.springs[1..].to_vec(),
+            groups: self.groups.clone(),
+        }
+    }
+
+    fn next_group(&self) -> Self {
+        let end = (self.groups[0] as usize + 1).min(self.springs.len());
+        Self {
+            springs: self.springs[end..].to_vec(),
+            groups: self.groups[1..].to_vec(),
+        }
+    }
+
+    fn unfold(self) -> Self {
+        let springs = self
+            .springs
+            .iter()
+            .cloned()
+            .chain(std::iter::once(Spring::Unknown))
+            .cycle()
+            .take(5 * self.springs.len() + 4)
+            .collect();
+        let groups = self
+            .groups
+            .iter()
+            .cloned()
+            .cycle()
+            .take(5 * self.groups.len())
+            .collect();
+        Self { springs, groups }
+    }
+}
+
+fn count_solutions(cache: &mut HashMap<Record, usize>, record: &Record) -> usize {
+    if let Some(&solutions) = cache.get(record) {
+        return solutions;
+    }
+
+    // no groups left + no damaged springs left = solved!
+    if record.groups.is_empty() {
+        let solutions = if record.springs.iter().all(|s| s != &Spring::Damaged) {
+            1
         } else {
-            return solutions;
-        }
+            0
+        };
+        cache.insert(record.clone(), solutions);
+        return solutions;
     }
-    let mut solutions = solutions;
-    for i in 0..springs.len() {
-        if springs[i] == Spring::Unknown {
-            springs[i] = Spring::Damaged;
-            let new_solutions = solve(springs, placeable - 1, solutions, groups);
-            if new_solutions > solutions {
-                solutions = new_solutions;
-            }
-            springs[i] = Spring::Operational;
-            let new_solutions = solve(springs, placeable, solutions, groups);
-            if new_solutions > solutions {
-                solutions = new_solutions;
-            }
-            springs[i] = Spring::Unknown;
-            return solutions;
-        }
+
+    // enough space left?
+    if record.springs.len() < record.groups.iter().sum::<u8>() as usize + record.groups.len() - 1 {
+        cache.insert(record.clone(), 0);
+        return 0;
     }
+
+    // skip operational spring
+    if record.springs[0] == Spring::Operational {
+        let solutions = count_solutions(cache, &record.next_spring());
+        cache.insert(record.clone(), solutions);
+        return solutions;
+    }
+
+    let mut solutions = 0;
+    // if next group fits, recurse
+    let group = record.groups[0] as usize;
+    let fits = record.springs[0..group]
+        .iter()
+        .all(|s| s != &Spring::Operational)
+        && ((record.springs.len() > group && record.springs[group] != Spring::Damaged)
+            || record.springs.len() == group);
+    // does this properly account for the case where the group exactly fits?
+    if fits {
+        solutions += count_solutions(cache, &record.next_group());
+    }
+
+    // also try skipping unknown
+    if record.springs[0] == Spring::Unknown {
+        solutions += count_solutions(cache, &record.next_spring());
+    }
+
+    cache.insert(record.clone(), solutions);
     solutions
 }
 
-fn is_solved(springs: &[Spring], groups: &[u8]) -> bool {
-    let (mut found, count) = springs
-        .iter()
-        .fold((vec![], 0_u8), |(mut acc, mut count), el| {
-            match el {
-                Spring::Damaged => count += 1,
-                _ => {
-                    if count > 0 {
-                        acc.push(count);
-                        count = 0;
-                    }
-                }
-            }
-            (acc, count)
-        });
-    if count > 0 {
-        found.push(count);
-    }
-
-    found == groups
-}
-
-fn unfold(springs: Vec<Spring>, groups: Vec<u8>) -> (Vec<Spring>, Vec<u8>) {
-    let springs = springs
-        .iter()
-        .cloned()
-        .chain(std::iter::once(Spring::Unknown))
-        .cycle()
-        .take(5 * springs.len() + 4)
-        .collect();
-    let groups = groups
-        .iter()
-        .cloned()
-        .cycle()
-        .take(5 * groups.len())
-        .collect();
-    (springs, groups)
-}
-
-type Line<'a> = (Vec<Spring>, Vec<u8>);
-
-fn parse_input(input: &str) -> IResult<&str, Vec<Line>> {
-    let (input, lines) = separated_list1(newline, parse_line)(input)?;
+fn parse_input(input: &str) -> IResult<&str, Vec<Record>> {
+    let (input, lines) = separated_list1(newline, parse_record)(input)?;
     Ok((input, lines))
 }
 
-fn parse_line(input: &str) -> IResult<&str, Line> {
+fn parse_record(input: &str) -> IResult<&str, Record> {
     // let (input, line) = terminated(many1(parse_span), nomchar(' '))(input)?;
     let (input, springs) = terminated(many1(parse_spring), nomchar(' '))(input)?;
     let (input, groups) = parse_groups(input)?;
-    Ok((input, (springs, groups)))
+    Ok((input, Record::new(springs, groups)))
 }
 
 fn parse_spring(input: &str) -> IResult<&str, Spring> {
@@ -134,42 +175,13 @@ mod tests {
     use super::*;
 
     #[test]
-    fn test_is_solved() {
-        let springs = vec![
-            Spring::Operational,
-            Spring::Damaged,
-            Spring::Damaged,
-            Spring::Damaged,
-            Spring::Operational,
-            Spring::Damaged,
-            Spring::Damaged,
-            Spring::Operational,
-            Spring::Damaged,
-            Spring::Operational,
-            Spring::Operational,
-            Spring::Operational,
-        ];
-        assert!(is_solved(springs.as_slice(), vec![3, 2, 1].as_slice()));
-    }
-
-    #[test]
-    fn temp() {
-        let input = "?###???????? 3,2,1";
-        // let input = ".###.##.? 3,2,1";
-        let (input, (mut springs, groups)) = parse_line(input).expect("Parsing error");
-        assert!(input.is_empty());
-        let result = solve(&mut springs, 3, 0, &groups);
-        assert_eq!(result, 10);
-    }
-
-    #[test]
     fn test_unfold() {
         let input = "???.### 1,1,3";
-        let (_, folded) = parse_line(input).unwrap();
+        let (_, folded) = parse_record(input).unwrap();
         let expected_input =
             "???.###????.###????.###????.###????.### 1,1,3,1,1,3,1,1,3,1,1,3,1,1,3";
-        let (_, expected) = parse_line(expected_input).unwrap();
-        let output = unfold(folded.0, folded.1);
+        let (_, expected) = parse_record(expected_input).unwrap();
+        let output = folded.unfold();
         assert_eq!(output, expected);
     }
 
